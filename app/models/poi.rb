ba406655edc0d376cb74b414df3556317485473c
Mike Jerome <jeromem@michrome.com>
2013-09-04 11:45:07 +0100

    Successfully read/write lon & lat from a POINT column

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -1,2 +1,15 @@
 class Poi < ActiveRecord::Base
+  attr_writer :lon, :lat
+
+  def lon
+    @lon ||= lonlat.try(:x)
+  end
+
+  def lat
+    @lat ||= lonlat.try(:y)
+  end
+
+  before_save do
+    self[:lonlat] = "POINT(#{lon} #{lat})"
+  end
 end
