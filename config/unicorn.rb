d0464e7d1fff90de6897b3366992cff34a9a1413
Mike Jerome <jeromem@michrome.com>
2013-09-04 13:41:56 +0100

    Update unicorn configuration to work with PostGIS

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,6 +17,9 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base) 
+    config = Rails.application.config.database_configuration[Rails.env] 
+    config['adapter'] = 'postgis' 
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
