262917dafd3dbfef4bd388530b3bc302e2809556
Mike Jerome <jeromem@michrome.com>
2013-09-07 15:20:54 +0100

    Default demo to Spain

--- app/controllers/nearby_searches_controller.rb
+++ app/controllers/nearby_searches_controller.rb
@@ -1,7 +1,7 @@
 class NearbySearchesController < ApplicationController
   def show
-    @poi = Poi.find(params[:poi_id] || 1)
-    @count = params[:count] || 9
+    @poi = Poi.find(params[:poi_id] || Poi.where(name: 'Plaza Mayor').first.id)
+    @count = params[:count] || 1
     @nearby_pois = @poi.nearest @count
   end
 end
